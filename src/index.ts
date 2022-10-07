c2ffee69c3c13e32128ce71d35ce7fcba95bd80f
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-10-08 02:21:03 +0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { Cookie, cookies } from './cookie.js'
+export type { CookieAttributes } from './types.js'
---
+++ src/types.ts
@@ -0,0 +1,31 @@
+export type Serialize = (value: any) => string | number | boolean
+export type Deserialize = (value: string) => any
+
+export interface CookieOptions {
+  /**
+   * @default
+   * return JSON.stringify(value)
+   */
+  serialize: Serialize
+
+  /**
+   * @default
+   * try {
+   *   return JSON.parse(value)
+   * } catch (err) {
+   *   console.error(err)
+   *   return null
+   * }
+   */
+  deserialize: Deserialize
+}
+
+export interface CookieAttributes {
+  path?: string
+  domain?: string
+  expires?: Date | number | string
+  secure?: boolean
+  maxAge?: number
+  'max-age'?: number
+  sameSite?: 'Lax' | 'Strict' | 'None'
+}
